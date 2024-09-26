2c0a29ea9548f17eb651bf27298d3c302680db9c
johnny <orgeorgij@gmail.com>
2024-09-26 22:16:02 +0300

    Initial commit for CW_PHP_2

---
+++ Index.php
@@ -0,0 +1,92 @@
+<?php
+    //bool
+    // $x = false;
+    // if ($x) {
+    //    echo "true";
+    // } else {
+    //     echo "false";
+    // }
+
+    //int
+    /*
+    $y = 15;
+    echo $y;
+    $x = 5;
+    $y = "H100";
+    echo $x + $y;
+    $num_10 = 25;
+    $num_2 = 0b101; //5
+    $num_8 = 035;
+    $num_16 = 0xFF25;
+    */ 
+
+    /*
+    //float
+    $a1 = 1.5;
+    $a2 = 1.3e4; // 1.3 * 10^4
+    */ 
+    
+    /*
+    //string
+    $a = 10;
+    $b = 5;
+    $result = "$a + $b<br>";
+    echo $result;
+    $result = '$a + $b<br>';
+    echo $result;
+    $result = '"Alex"<br>';
+    echo $result;
+    $result = "'Alex'<br>";
+    echo $result;
+    */ 
+
+    /*
+    $a = null;
+    echo $a;
+    */ 
+    
+    /*
+    define("STEP", 19);
+    const PI = 3.14;
+    echo PI."<hr>";
+    echo STEP."<hr>";
+    echo __LINE__."<hr>";
+    echo __FILE__."<hr>";
+    echo __DIR__."<hr>"; // PHP 5.3 and later
+    */ 
+    
+    /*
+    $x = 10;
+    $y = "10";
+    $result = $x !== $y;
+    */ 
+    
+    /*
+    $a = 2 <=> 2; // 2 == 2
+    $b = 3 <=> 2; // 3 > 2
+    $c = 1 <=> 2; // 3 < 2
+    echo $a." ".$b." ".$c;
+    (&& and), (|| or), (!)
+    $result = 5; //false
+    $result = 15 and 10;
+    $result = 0 or 5;
+    $result = (true xor true); // false
+    echo $result;
+    $result = (5 ^ 4); // 101 ^ 100 = 1
+    echo $result;
+    */ 
+    
+    /*
+    $x = 5;
+    ++$x; //6
+    $x++; //7
+    echo $x++;
+    echo $x;
+    //8
+    */ 
+    
+    /*
+    
+    */ 
+    
+?>
